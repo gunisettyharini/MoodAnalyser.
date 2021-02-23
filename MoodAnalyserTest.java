f4211c8a8b985393312513628d423b97ca9e93bb
Harini Gunisetty <gunisettyharini@gmail.com>
2021-02-24 04:19:56 +0530

    Touse enum to throw exception

--- MoodAnalyserTest.java
+++ MoodAnalyserTest.java
@@ -1,10 +1,32 @@
-import org.junit.Assert;
 import org.junit.Test;
+import org.junit.Assert;
+import org.junit.rules.ExpectedException;
 
-public class MoodanalyserTest {
-    static MoodAnalyser checkMessage = new MoodAnalyser(null);
+class MoodAnalyserTest {
+    static String mood = null;
     @Test
-    public void givenNullmoodShouldReturnHappy() {
-        Assert.assertEquals("HAPPY", checkMessage.analyseMood());
+    public void givenMessage_whenNull_ShouldThrowException() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+        try {
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(MoodAnalyserException.class);
+            mood = moodAnalyser.analyseMood();
+            Assert.assertEquals("HAPPY",mood);
+        } catch (MoodAnalyserException e) {
+            Assert.assertEquals(MoodAnalyserException.ExceptionType.ENTERED_NULL, e.type);
+        }
+    }
+
+    @Test
+    public void givenMessage_whenEmpty_ShouldThrowException() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("");
+        try {
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(MoodAnalyserException.class);
+            mood = moodAnalyser.analyseMood();
+            Assert.assertEquals("HAPPY",mood);
+        } catch (MoodAnalyserException e) {
+            Assert.assertEquals(MoodAnalyserException.ExceptionType.ENTERED_EMPTY, e.type);
+        }
     }
 }
