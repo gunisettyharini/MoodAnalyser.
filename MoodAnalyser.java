956ff0651773906efcd7583a77087ec068ee9f98
Harini Gunisetty <gunisettyharini@gmail.com>
2021-02-24 04:14:17 +0530

    To use enum to throw exception

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -1,10 +1,14 @@
 public class MoodAnalyser {
     public String message;
+
     public MoodAnalyser(String message) {
         this.message = message;
     }
-    public String analyseMood() {
+
+    public String analyseMood() throws MoodAnalyserException {
         try {
+            if (message.isEmpty())
+                throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.ENTERED_EMPTY, "Please enter proper message");
             if (message.contains("sad"))
                 return "SAD";
             return "HAPPY";
